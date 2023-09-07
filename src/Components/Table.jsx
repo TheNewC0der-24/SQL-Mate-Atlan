465fdea7ec05d429e9ae7221a0c90040b65b20d4
Bhavya Khurana <bhavyakhurana002@gmail.com>
2023-09-08 01:44:51 +0530

    Output

---
+++ src/Components/Table.jsx
@@ -0,0 +1,46 @@
+import { Box, Typography } from "@mui/material";
+
+import TableUI from "./Common/TableUI";
+
+const Table = ({ query, headers, rows }) => {
+
+  return (
+    <Box>
+      {query ? (
+        <Box sx={{
+          mt: 5,
+          mb: 5,
+          bgcolor: "#fff",
+          borderRadius: "10px",
+          p: 2,
+        }}>
+          <Typography variant="h5" sx={{ fontWeight: "bold" }}>
+            Output
+          </Typography>
+          <TableUI headers={headers} rows={rows} query={query} />
+
+        </Box>
+      ) : (
+        <Box
+          sx={{
+            mt: 5,
+            mb: 5,
+            bgcolor: "#fff",
+            borderRadius: "10px",
+            p: 2,
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "200px",
+          }}
+        >
+          <Typography variant="h5" color="#">
+            Run the selected Query to see the output !!
+          </Typography>
+        </Box>
+      )}
+    </Box>
+  );
+}
+
+export default Table;
